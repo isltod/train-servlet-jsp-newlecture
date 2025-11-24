97d51b8aa1cf53ab532b2cd19d1e09ba5ab98595
E. K. Lee <wolf@teoal.net>
2025-11-25 07:16:27 +0900

    쿠키 저장 실습 완료

--- Prj10-StateApplication/src/main/java/com/newlecture/web/Calc.java
+++ Prj10-StateApplication/src/main/java/com/newlecture/web/Calc.java
@@ -2,10 +2,12 @@ package com.newlecture.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,27 +22,46 @@ public class Calc extends HttpServlet {
 		
 		PrintWriter out = resp.getWriter();
 		
+//		ServletContext application = req.getServletContext();
+//		HttpSession session = req.getSession();
+		Cookie[] cookies = req.getCookies();
+		
 		String operator = req.getParameter("operator");
 		String v_ = req.getParameter("v");
-		
-//		ServletContext application = req.getServletContext();
-		HttpSession session = req.getSession();
 		
 		int v = 0;
 		if (!v_.equals("")) v = Integer.parseInt(v_);
 		int result = 0;
 		if (operator.equals("=")) {
 //			int first_v = (Integer) application.getAttribute("1st_value");
-			int first_v = (Integer) session.getAttribute("1st_value");
 //			String first_op = (String) application.getAttribute("1st_operator");
-			String first_op = (String) session.getAttribute("1st_operator");
+//			int first_v = (Integer) session.getAttribute("1st_value");
+//			String first_op = (String) session.getAttribute("1st_operator");
+			int first_v = 0;
+			for (Cookie cookie :cookies) {
+				if (cookie.getName().equals("value")) {
+					first_v = Integer.parseInt(cookie.getValue());
+					break;
+				}
+			}
+			String first_op = "";
+			for (Cookie cookie :cookies) {
+				if (cookie.getName().equals("operator")) {
+					first_op = cookie.getValue();
+					break;
+				}
+			}
 			result = first_op.equals("+") ? first_v + v : first_v - v;
 			out.printf("%s 결과는 %d입니다.\n", operator, result);
 		} else {
 //			application.setAttribute("1st_value", v);
 //			application.setAttribute("1st_operator", operator);
-			session.setAttribute("1st_value", v);
-			session.setAttribute("1st_operator", operator);
+//			session.setAttribute("1st_value", v);
+//			session.setAttribute("1st_operator", operator);
+			Cookie valCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("operator", operator);
+			resp.addCookie(opCookie);
+			resp.addCookie(valCookie);
 		}
 	}
 }
