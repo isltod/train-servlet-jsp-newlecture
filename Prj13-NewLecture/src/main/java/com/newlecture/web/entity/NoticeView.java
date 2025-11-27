bc3ec2f1fd47ea51a65cc2f8c1985b33ac0fe7ba
E. K. Lee <wolf@teoal.net>
2025-11-28 02:01:04 +0900

    service, 페이징, 댓글 수 실습 완료

---
+++ Prj13-NewLecture/src/main/java/com/newlecture/web/entity/NoticeView.java
@@ -0,0 +1,22 @@
+package com.newlecture.web.entity;
+
+import java.util.Date;
+
+public class NoticeView extends Notice {
+	private int cmtCount;
+
+	public NoticeView(int id, String title, Date regDate, String writer_id, int hit, String files, int cmtCount) {
+		super(id, title, regDate, writer_id, hit, files, "");
+		this.cmtCount = cmtCount;
+	}
+
+	public int getCmtCount() {
+		return cmtCount;
+	}
+
+	public void setCmtCount(int cmtCount) {
+		this.cmtCount = cmtCount;
+	}
+	
+	
+}
